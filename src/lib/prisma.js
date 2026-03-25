58ef3fe433d14e4136bb98a8581365efcf5563c7
endodo <kuehnp@bzz.ch>
2026-03-25 09:33:59 +0100

    primsa 7.5 changes

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } },
-})
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 export default prisma
